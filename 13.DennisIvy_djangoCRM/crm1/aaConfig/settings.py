077bcbb61519af16aa3483fd24e79e6b02932246
MAREK <marekidziak1@gmail.com>
2022-04-28 21:48:36 +0300

    database commit

--- 13.DennisIvy_djangoCRM/crm1/aaConfig/settings.py
+++ 13.DennisIvy_djangoCRM/crm1/aaConfig/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- 13.DennisIvy_djangoCRM/crm1/accounts/views.py
+++ 13.DennisIvy_djangoCRM/crm1/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
 
 def home(request):
     context={}
@@ -9,7 +9,8 @@ def contact(request):
     context={}
     return render(request, 'accounts/contact.html', context)
 def products(request):
-    context={}
+    products = Product.objects.all()
+    context={'products':products}
     return render(request, 'accounts/products.html', context)
 def customer(request):
     context={}
